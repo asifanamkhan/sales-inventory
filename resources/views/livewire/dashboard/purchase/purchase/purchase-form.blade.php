89e6e0d0ff5eeffd1c4a13ad96021f463acbd268
unknown <somuddro249@gmail.com>
2024-07-02 15:25:56 +0600

    purchase uprate

--- resources/views/livewire/dashboard/purchase/purchase/purchase-form.blade.php
+++ resources/views/livewire/dashboard/purchase/purchase/purchase-form.blade.php
@@ -12,7 +12,7 @@
         {{ session('status') }}
     </div>
     @elseif (session('error'))
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
         {{ session('error') }}
     </div>
     @endif
@@ -161,14 +161,14 @@
                 <table class="table table-bordered">
                     <thead>
                         <tr class="bg-sidebar">
-                            <td class="" style="width:5%">SL</td>
+                            <td class="" style="width:2%">SL</td>
                             <td class="" style="width:30%">Name</td>
                             <td class="text-center" style="width:10%">Qty</td>
                             <td class="text-center" style="width:15%">Price</td>
                             <td class="text-center" style="width:10%">Discount</td>
-                            <td class="text-center" style="width:10%">Tax</td>
+                            <td class="text-center" style="width:15%">Tax</td>
                             <td class="text-center" style="width:15%">Total Amount</td>
-                            <td class="text-center" style="width:5%">Action</td>
+                            <td class="text-center" style="width:2%">Action</td>
                         </tr>
                     </thead>
                     <tbody >
@@ -208,14 +208,14 @@
                             </td>
                             <td>
                                 <div class="text-center">
-                                    <button type="button" wire:click.prevent=''>
+                                    <a type="button" wire:click.prevent='removeItem({{ $purchase_key}}, {{$purchase['st_group_item_id'] }})'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red"
                                             class="dz-w-6 dz-h-6 dz-text-black dark:dz-text-white">
                                             <path fill-rule="evenodd"
                                                 d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
                                                 clip-rule="evenodd"></path>
                                         </svg>
-                                    </button>
+                                    </a>
                                 </div>
                             </td>
                         </tr>
@@ -224,12 +224,66 @@
                         @endforelse
                     </tbody>
                     <tfoot>
-                        <tr>
-                            <td colspan="2" style="text-align: right">Total qty:</td>
-                            <td style="text-align: center">20</td>
-                            <td colspan="3" style="text-align: right">Total amount: </td>
-                            <td style="text-align: right">20</td>
+                        <tr style="font-weight: 500; background:aliceblue">
+                            <td colspan="2" style="text-align: right">Total:</td>
+                            <td style="text-align: center">
+                                {{ $state['total_qty'] }}
+                            </td>
+                            <td colspan="1" style="text-align: right"></td>
+                            <td style="text-align: center">
+                                {{ $state['tot_discount'] }}
+                            </td>
+                            <td colspan="1" style="text-align: right">Sub total: </td>
+                            <td style="text-align: right">
+                                {{ $state['net_payable_amt'] }}
+                            </td>
+                            <td></td>
                         </tr>
+                        <tr>
+                            <td rowspan="4" colspan="5" style="border: none">
+
+                            </td>
+                            <td style="text-align: right">
+                                Shipping
+                            </td>
+                            <td>
+                                <input wire:model='state.shipping_amt' style="text-align: right" class="form-control"
+                                    wire:input.debounce.300ms='grandCalculation'>
+                            </td>
+                            <td></td>
+                        </tr>
+
+                        <tr >
+                            <td style="text-align:right">
+                                Net payable
+                            </td>
+                            <td style="text-align:right">
+                                <input style="text-align: right" readonly class="form-control"
+                                    wire:model='state.tot_payable_amt'>
+                            </td>
+                            <td style=""></td>
+                        </tr>
+                        <tr >
+                            <td style="text-align:right">
+                                 Payment amount
+                            </td>
+                            <td style="text-align:right">
+                                <input style="text-align: right" class="form-control"
+                                    wire:model='state.pay_amt' wire:input.debounce.300ms='grandCalculation'>
+                            </td>
+                            <td style=""></td>
+                        </tr>
+                        <tr style="border-bottom: none">
+                            <td style="border-bottom: 1px solid #DEE2E6; text-align:right">
+                                 Due amount
+                            </td>
+                            <td style="border-bottom: 1px solid #DEE2E6; text-align:right">
+                                <input style="text-align: right" readonly class="form-control"
+                                    wire:model='state.due_amt'>
+                            </td>
+                            <td style="border-bottom: 1px solid #DEE2E6"></td>
+                        </tr>
+
                     </tfoot>
                 </table>
             </div>
